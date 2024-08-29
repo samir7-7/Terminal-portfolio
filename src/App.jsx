c5f90f29a7c62c11c1db24ed7b0caa0affe0e821
Samir Nepal <samirnepal9817@gmail.com>
2024-08-29 23:13:55 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className='bg-black w-[100vw] h-[100vh] flex justify-center items-center'>
+        <div className="screen bg-gray-800 border-[4px] border-gray-50 h-[60%] w-[90%] md:h-[90%] rounded-xl">
+            <Navbar/>
+           
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between px-5 items-center h-[5vh] bg-gray-300 rounded-t-lg relative w-full'>
+      <div className="buttons flex gap-2 items-center">
+        <div className="red bg-red-500 w-4 h-4 rounded-full cursor-pointer hover:bg-red-600 hover:translate-y-[-2px] duration-100"></div>
+        <div className="yellow bg-yellow-500 w-4 h-4 rounded-full cursor-pointer hover:bg-yellow-600 hover:translate-y-[-2px] duration-100"></div>
+        <div className="green bg-green-500 w-4 h-4 rounded-full cursor-pointer hover:bg-green-600 hover:translate-y-[-2px] duration-100"></div>
+        <div className='w-[150px] h-[25px]'></div>
+        <div className="bar md:flex  hidden bg-gray-800 h-[25px] w-[120px] z-10 absolute bottom-0 left-28 rounded-t-md text-center font-semibold text-[10px] text-white items-center justify-between px-2">
+          Terminal
+         
+          <div className='w-3 h-3 bg-gray-100 flex justify-center items-center rounded-full'>
+            <svg className='w-2 h-2' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#2c2947" d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" /></svg>
+          </div>
+        </div>
+        <div className="newtab absolute left-60 bottom-1 md:flex  hidden items-end justify-center h-[25px] w-[25px]  cursor-pointer ">
+        <svg className='w-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#858585" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
+        </div>
+
+      </div>
+      <div className="icon">
+        <a href="https://github.com/samir7-7" target='_blank'>
+          <svg className='w-5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="#383839" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" /></svg>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
